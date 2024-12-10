8153f94070ecc0200939197ff1c263420bb33d0d
SuperbVisor <144216485+SuperbVisor@users.noreply.github.com>
2024-12-11 00:15:37 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'db_webfp')
